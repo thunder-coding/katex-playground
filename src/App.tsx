ee878788b54575791d8995ee916cdb3573a416c0
Yaksh Bariya <yakshbari4@gmail.com>
2022-08-13 16:45:16 +0530

    initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, Fragment } from "react";
+import "./App.css";
+import "katex/dist/katex.css";
+import download from "downloadjs";
+import * as htmlToImage from "html-to-image";
+import katex from "katex";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="textarea-wrapper">
+      <textarea
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+          let err = "";
+          try {
+            setOutput(
+              (err = katex.renderToString(event.target.value, {
+                throwOnError: true,
+              }))
+            );
+          } catch (err) {
+            setOutput(
+              (err as unknown as Error).message
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+            );
+          }
+        }}
+      />
+      <div id="output" dangerouslySetInnerHTML={{ __html: output }}></div>
+      <button
+        onClick={() => {
+          htmlToImage
+            .toJpeg(document.getElementById("output") as HTMLElement, {
+              backgroundColor: "white",
+              cacheBust: true,
+            })
+            .then((dataURL) => {
+              let img = new Image();
+              img.src = dataURL;
+              download(dataURL, "image.jpg", "data:image/jpeg;base64");
+            })
+            .catch((err) => {
+              alert("fuck");
+            });
+        }}
+      >
+        Download
+      </button>
     </div>
   );
 }
